38ad371fcfab6c6b5c3419491beb569c543fef25
Greg Eure <geure@hawk.iit.edu>
2020-04-17 17:05:10 -0500

    Added jquery slider
    Added all code for jquery slider

---
+++ js/slider.script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	$('.bxslider').bxSlider({
+  auto: true,
+  autoControls: true,
+  stopAutoOnClick: true,
+  pager: true,
+  mode: 'fade',
+  captions: true,
+  slideWidth: 500,
+  pagerCustom: '#bx-pager',
+  adaptiveHeight: true,
+  adaptiveHeightSpeed: 500
+	});
+});
